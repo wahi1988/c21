6c8bf5f7e8c54cbeaf073bcf1dd38a6374fc868a
wahi1988 <73730696+wahi1988@users.noreply.github.com>
2020-11-12 12:10:10 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+var fixedRect, movingRect;
+var car, wall;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(600, 400, 50, 80);
+  fixedRect.shapeColor = "green";
+  
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "green";
+
+  car = createSprite(100,100,50,50)
+  car.shapeColor = "blue";
+
+  wall = createSprite(800,100,50,80)
+  wall.shapeColor = "pink";
+  car.velocityX=5;
+}
+
+function draw() {
+  background(0,0,0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+  if(isTouching(car, wall)){
+    movingRect.shapeColor = "red";
+    fixedRect.shapeColor = "red";
+  }
+  else {
+    movingRect.shapeColor = "green";
+    fixedRect.shapeColor = "green";
+  }
+
+  bounceOff(car,wall)
+  drawSprites();
+}
